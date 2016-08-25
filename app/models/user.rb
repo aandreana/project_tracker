e8cec910e4afc5c37479eff8d16209b17cbd8246
antand <tony.andreana@rogers.com>
2016-08-25 00:12:38 +0000

    Update test to deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   def full_name
-    "#{first_name.capitalize} #{last_name.capitalize}"
+    "#{first_name} #{last_name}"
   end
 end
