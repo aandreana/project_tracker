92a3bcafce13db87123661a358a3953e6a6c1577
antand <tony.andreana@rogers.com>
2016-08-25 01:53:35 +0000

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User < ApplicationRecord
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine::cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
